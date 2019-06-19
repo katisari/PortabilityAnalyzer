1be03657aeb0ac0aa29717b6e48fbee7e799db0f
Ciara Hawkins <33327680+cmhawkins@users.noreply.github.com>
2019-06-19 16:14:52 -0700

    "Added UI configurations for LIbba"

--- PortAPIUI/MsBuildAnalyzer.cs
+++ PortAPIUI/MsBuildAnalyzer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Documents;
@@ -11,6 +12,18 @@ namespace PortAPIUI
         public static List<string> GetAssemblies(string projectPath)
         {
             MessageBox.Show("Hi from Libba");
+            GetConfig();
+            GetPlatform();
+            return new List<string>();
+        }
+
+        internal static List<string> GetConfig()
+        {
+            return new List<string>();
+        }
+
+        internal static List<string> GetPlatform()
+        {
             return new List<string>();
         }
     }
