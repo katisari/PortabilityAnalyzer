0c7dc3b050548e5793561c27f65c6f7ba3ee1406
Katie Lee <ktjlee@umich.edu>
2019-06-23 23:26:17 -0700

    connected export functionality to ui

--- PortAPIUI/ApiAnalyzer.cs
+++ PortAPIUI/ApiAnalyzer.cs
@@ -1,99 +1,117 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 using System.Text;
 using System.Windows;
 using System.Windows.Documents;
+using System.Windows.Threading;
 
 namespace PortAPIUI
 {
     class ApiAnalyzer
     {
+        // gets input path of csproj file
+        public static string ReportingPath;
+        public static string InputPath;
         public static void AnalyzeAssemblies(List<string> assemblies)
         {
-            MessageBox.Show("Hi from Katie");
-        }
-//        var ourPath = Environment.GetCommandLineArgs()[0];
-//        var ourDirectory = System.IO.Path.GetDirectoryName(ourPath);
-//        var parentPath = System.IO.Directory.GetParent(ourDirectory);
-//        //var grandma = System.IO.Directory.GetParent(parentPath);
-//        //var grandparentPath = System.IO.Directory.GetParent(parentPath);
-//        var apiPortPath = System.IO.Path.Combine(ourDirectory, "ApiPort", "net461", "ApiPort.exe");
-//        //var apiPortPath = System.IO.Path.Combine(System.IO.Directory.GetParent(), "dotnet.exe");
+        //    MessageBox.Show("Hi from Katie");
+        //    string ourPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
+        //    string apiDllPath = System.IO.Path.Combine(ourPath, "ApiPort", "netcoreapp2.1", "ApiPort.dll");
+        //    string InputPathParent = System.IO.Directory.GetParent(InputPath).FullName;
 
-//        var p = new Process();
-//        p.StartInfo.FileName = apiPortPath;
+        //    Process p = new Process();
 
-//        //var reportPath = GenerateReportPath();
+        //    p.StartInfo.FileName = "dotnet.exe";
+        //    string reportPath = GenerateReportPath();
 
-//        //output to Desktop
+        //    //output to Desktop
 
-//        p.StartInfo.Arguments = $"analyze -f \"C:\\Users\\t-jaele\\Downloads\\Paint\" -o \"C:\\Users\\t-jaele\\Desktop\\ApiPortResults\" -t \".NET Core, Version=3.0\"";
-//        var Hello = p.StartInfo.Arguments;
-//        p.StartInfo.CreateNoWindow = true;
-//        p.StartInfo.UseShellExecute = false;
-//        p.StartInfo.RedirectStandardOutput = true;
-//        p.EnableRaisingEvents = true;
+        //    p.StartInfo.Arguments = $"{apiDllPath} analyze -f \"{InputPathParent}\" -o \"{reportPath}\" -t \".NET Core, Version=3.0\"";
+        //    var Hello = p.StartInfo.Arguments;
+        //    p.StartInfo.CreateNoWindow = true;
+        //    p.StartInfo.UseShellExecute = false;
+        //    p.StartInfo.RedirectStandardOutput = true;
+        //    p.EnableRaisingEvents = true;
 
-//        List<string> msg = new List<string>();
+        //    List<string> msg = new List<string>();
 
-//        p.OutputDataReceived += (s, o) =>
-//        {
-//            if (o.Data != null)
-//            {
-//                Dispatcher.Invoke(() =>
-//                {
-//                    msg.Add(o.Data);
-//                });
+        //    p.OutputDataReceived += (s, o) =>
+        //    {
+        //        if (o.Data != null)
+        //        {
+        //            Application.Current.Dispatcher.Invoke(() =>
+        //            {
+        //                msg.Add(o.Data);
+        //            });
 
-//            }
-//            //AnalzeBtn.IsEnabled = true;
-//        };
+        //        }
+        //        //AnalzeBtn.IsEnabled = true;
+        //    };
 
-//        p.Exited += delegate
-//        {
-//            Dispatcher.Invoke(() =>
-//            {
-               
+        //    p.Exited += delegate
+        //    {
+        //        Application.Current.Dispatcher.Invoke(() =>
+        //        {
+        //            string text;
 
-//                string text;
+        //            if (msg.Count != 17) // Was not successful
+        //            {
 
-//                if (msg.Count != 17) // Was not successful
-//                {
-//                    //OpenReportButton.Visible = false;
+        //                if (msg.Count < 10) // Exception was thrown in the API console tool
+        //                {
+        //                    text = $"Unable to analyze. The access to the specified path might be denied.";
+        //                }
+        //                else
+        //                {
+        //                    text = msg.FindLast(o => !String.IsNullOrEmpty(o));
+        //                    text = text.Trim(new char[] { '*', ' ' });
+        //                    if (!text.Equals("No files were found to analyze.", StringComparison.InvariantCultureIgnoreCase))
+        //                    {
+        //                        msg.RemoveRange(0, 10);
+        //                        var details = String.Join(Environment.NewLine, msg.ToArray());
+        //                        text = $"Unable to analyze.{Environment.NewLine}Details:{Environment.NewLine}{details}";
+        //                    }
+        //                }
+        //            }
+        //            else // Was successful
+        //            {
+        //                //text = $"Report saved in: {Environment.NewLine}{reportPath}";
+        //                //OpenReportButton.Visible = true;
+        //            }
 
-//                    if (msg.Count< 10) // Exception was thrown in the API console tool
-//                    {
-//                        text = $"Unable to analyze. The access to the specified path might be denied.";
-//                    }
-//                    else
-//                    {
-//                        text = msg.FindLast(o => !String.IsNullOrEmpty(o));
-//                        text = text.Trim(new char[] { '*', ' ' });
-//                        if (!text.Equals("No files were found to analyze.", StringComparison.InvariantCultureIgnoreCase))
-//                        {
-//                            msg.RemoveRange(0, 10);
-//                            var details = String.Join(Environment.NewLine, msg.ToArray());
-//text = $"Unable to analyze.{Environment.NewLine}Details:{Environment.NewLine}{details}";
-//                        }
-//                    }
-//                }
-//                else // Was successful
-//                {
-//                    //text = $"Report saved in: {Environment.NewLine}{reportPath}";
-//                    //OpenReportButton.Visible = true;
-//                }
+        //            //OutputTextBox.Text = text;
+        //            //UIline1.Visible = true;
+        //        });
 
-//                //OutputTextBox.Text = text;
-//                //UIline1.Visible = true;
-//            });
+        //    };
 
-//        };
+        //    p.Start();
 
-//        p.Start();
+        //    //UIline1.Visible = true;
+        //    //OutputTextBox.Text = "Analyzing...";
 
-//        //UIline1.Visible = true;
-//        //OutputTextBox.Text = "Analyzing...";
+        //    p.BeginOutputReadLine();
 
-//        p.BeginOutputReadLine();
+        //}
+        //private static string GenerateReportPath()
+        //{
+        //    var outputDirectory = System.IO.Path.GetTempPath();
+        //    var outputName = "PortabilityReport";
+        //    var outputExtension = ".xlsx";
+        //    var counter = 1;
+        //    var outputPath = System.IO.Path.Combine(outputDirectory, outputName + outputExtension);
+
+        //    while (File.Exists(outputPath))
+        //    {
+        //        outputPath = System.IO.Path.Combine(outputDirectory, $"{outputName}({counter}){outputExtension}");
+        //        counter++;
+        //    }
+
+        //    return outputPath;
+        //}
     }
 }
+
